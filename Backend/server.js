e85481790ed78f75e58f558656bb546a03f1dd92
Sahil Chaudhary <sahilchaudhary20020128@gmail.com>
2024-05-21 12:44:40 +0530

    edit package.json

--- Backend/server.js
+++ Backend/server.js
@@ -24,10 +24,10 @@ app.use(cors());
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
 });
     app.listen(PORT,()=>{
         console.log(`Server Started on http://localhost:${PORT}`);
