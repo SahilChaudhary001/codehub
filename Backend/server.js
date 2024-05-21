319bd7013a02033252a8e1aafa4266154e206f39
Sahil Chaudhary <sahilchaudhary20020128@gmail.com>
2024-05-21 13:01:54 +0530

    remove cors

--- Backend/server.js
+++ Backend/server.js
@@ -5,21 +5,21 @@ import passport from "passport";
 import session from "express-session";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.routes.js";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import exploreRoutes from "./routes/explore.route.js";
 import path from "path"
-import cors from "cors";
+// import cors from "cors";
 import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 const app=   express();
 const PORT=process.env.PORT || 5000 ;
 const __dirname=path.resolve();
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors());
+// app.use(cors());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
